398642b00c3907abb67091a1606259443da79e5e
Dominic A.N. <dominicnunez@gmail.com>
2024-10-16 12:08:56 -0500

    create thunk for api logic

---
+++ frontend/state/thunk.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const URL = "http://localhost:9009/api/pizza";
+
+const handleResponse = async (response) => {
+  if (!response.ok) {
+    const errorData = await response.json();
+    const errorMessage = errorData.message || "Failed to fetch pizzas";
+    throw new Error(errorMessage);
+  }
+  return await response.json();
+};
+
+export const fetchOrderHistory = createAsyncThunk(
+  "pizzas/fetchOrderHistory",
+  async () => {
+    const response = await fetch(`${URL}/history`);
+    await handleResponse(response);
+    return await response.json();
+  }
+);
+
+export const submitOrder = createAsyncThunk(
+  "pizzas/submitOrder",
+  async (order) => {
+    const response = await fetch(`${URL}/order`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    });
+
+    await handleResponse(response);
+  }
+);
