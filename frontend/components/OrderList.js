10793a17c0c9b42d46b726c3f78c0c54fe79a27a
Dominic A.N. <dominicnunez@gmail.com>
2024-10-23 11:25:05 -0500

    Add "no toppings" possibility to order list view

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,60 +1,66 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { historyState, filterState } from '../state/selectors'
-import { fetchOrderHistory } from '../state/thunk'
-import { setFilter } from '../state/slice'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { historyState, filterState } from "../state/selectors";
+import { fetchOrderHistory } from "../state/thunk";
+import { setFilter } from "../state/slice";
 
 export default function OrderList() {
-  const dispatch = useDispatch()
-  const orders = useSelector(historyState)
-  const filter = useSelector(filterState)
+  const dispatch = useDispatch();
+  const orders = useSelector(historyState);
+  const filter = useSelector(filterState);
 
   useEffect(() => {
     dispatch(fetchOrderHistory());
   }, [dispatch]);
 
-  const filteredOrders = filter == "All" ? orders : orders.filter(order => order.size == filter);
+  const filteredOrders =
+    filter == "All" ? orders : orders.filter((order) => order.size == filter);
 
-  const handleFilterChange = size => {
-    dispatch(setFilter(size))
-  }
+  const handleFilterChange = (size) => {
+    dispatch(setFilter(size));
+  };
 
   return (
     <div id="orderList">
       <h2>Pizza Orders</h2>
       <ol>
-        {
-          filteredOrders.map((order, index) => {
-            // console.log({"current order map": order})
-            const toppings = order.toppings || []; // Use an empty array if toppings is undefined
-            const toppingCount = toppings?.length || 0;
-            const toppingText = toppingCount ==  1 ? "1 topping" : `${toppingCount} toppings`
-            
-            return (
-              <li key={index}>
-                <div>
-                  {order.customer} ordered a size {order.size} with {toppingText}
-                </div>
-              </li>
-            )
-          })
-        }
+        {filteredOrders.map((order, index) => {
+          // console.log({"current order map": order})
+          const toppings = order.toppings || []; // Use an empty array if toppings is undefined
+          const toppingCount = toppings?.length || 0;
+          // const toppingText = toppingCount ==  1 ? "1 topping" : `${toppingCount} toppings`
+          const toppingText =
+            toppingCount === 0
+              ? "no toppings"
+              : toppingCount === 1
+              ? "1 topping"
+              : `${toppingCount} toppings`;
+
+          return (
+            <li key={index}>
+              <div>
+                {order.customer} ordered a size {order.size} with {toppingText}
+              </div>
+            </li>
+          );
+        })}
       </ol>
       <div id="sizeFilters">
         Filter by size:
-        {
-          ['All', 'S', 'M', 'L'].map(size => {
-            const className = `button-filter${size === filter ? ' active' : ''}`
-            return <button
+        {["All", "S", "M", "L"].map((size) => {
+          const className = `button-filter${size === filter ? " active" : ""}`;
+          return (
+            <button
               data-testid={`filterBtn${size}`}
               className={className}
               key={size}
-              onClick={() => handleFilterChange(size)}>
-                {size}
-              </button>
-          })
-        }
+              onClick={() => handleFilterChange(size)}
+            >
+              {size}
+            </button>
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
